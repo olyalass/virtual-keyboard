2f333a2b6153bbd0d4c50fc23dface1bf421cf68
olyalass <olyalass@icloud.com>
2023-04-30 23:35:39 +0200

    feat: complete keyboard design and create array with key objs

--- src/components/key/index.js
+++ src/components/key/index.js
@@ -3,15 +3,35 @@ import "./style.scss";
 export class Key {
   container;
 
-  constructor(name) {
+  constructor(keyObj) {
     this.container = document.createElement("div");
     this.container.classList.add("key__container");
     const face = document.createElement("div");
     face.classList.add("key__face");
     const text = document.createElement("p");
     text.classList.add("key__text");
-    text.textContent = name;
+    keyObj.type === "char"
+      ? (text.textContent = keyObj.key)
+      : keyObj.name !== undefined
+      ? (text.textContent = keyObj.name)
+      : (text.textContent = keyObj.code);
     face.append(text);
+    if (keyObj.size) {
+      keyObj.size === 2
+        ? this.container.classList.add("key__container_s")
+        : keyObj.size === 3
+        ? this.container.classList.add("key__container_event_m")
+        : keyObj.size === 4
+        ? this.container.classList.add("key__container_event_l")
+        : keyObj.size === 5
+        ? this.container.classList.add("key__container_event_xl")
+        : this.container.classList.add("key__container_xs");
+    }
     this.container.append(face);
+    if (keyObj.type === "event") {
+      keyObj.isArrow
+        ? this.container.classList.add("key__container_arrow")
+        : this.container.classList.add("key__container_event");
+    }
   }
 }
--- src/components/keyboard/index.js
+++ src/components/keyboard/index.js
@@ -1,5 +1,6 @@
 import "./style.scss";
 import { Key } from "../key";
+import keys from "./keys.json";
 
 export class Keyboard {
   container;
@@ -7,9 +8,14 @@ export class Keyboard {
   constructor() {
     this.container = document.createElement("div");
     this.container.classList.add("keyboard__container");
-    const key = new Key("S");
-    const keyTwo = new Key("U");
-    const keyThree = new Key("R");
-    this.container.append(key.container, keyTwo.container, keyThree.container);
+    keys.forEach((e) => {
+      let key;
+      e.type === "char"
+        ? (key = new Key(e))
+        : e.name
+        ? (key = new Key(e))
+        : (key = new Key(e));
+      this.container.append(key.container);
+    });
   }
 }
